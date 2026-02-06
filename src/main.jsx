90f88ffd13d147f0aedbb9b3fc41448c54b9e103
sinjitonayo <tanakashinji23@gmail.com>
2026-02-06 19:41:16 +0800

    feat(auth): add register page

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,18 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
-import App from './App.jsx'
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import Layout from './components/Layout.js'
+import UserRegister from './components/User/UserRegister.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    <BrowserRouter>
+      <Routes>
+        <Route element={<Layout />}>
+          <Route path="/register" element={<UserRegister />} />
+        </Route>
+      </Routes>
+    </BrowserRouter>
   </StrictMode>,
 )
