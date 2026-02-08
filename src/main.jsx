23ffbcb89fdc5494adcbfa833621ea9faf4fe665
sinjitonayo <tanakashinji23@gmail.com>
2026-02-08 21:24:27 +0800

    feat: add contact creation form with its functionality and UI

--- src/main.jsx
+++ src/main.jsx
@@ -1,25 +1,33 @@
-import {StrictMode} from 'react'
-import {createRoot} from 'react-dom/client'
-import {BrowserRouter, Route, Routes} from "react-router";
+import { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
+import { BrowserRouter, Route, Routes } from "react-router";
 import Layout from "./components/Layout.jsx";
 import UserRegister from "./components/User/UserRegister.jsx";
 import UserLogin from "./components/User/UserLogin.jsx";
 import DashboardLayout from "./components/DashboardLayout.jsx";
 import UserProfile from './components/User/UserProfile.jsx';
 import UserLogout from './components/User/UserLogout.jsx';
+import ContactCreate from './components/Contact/ContactCreate.jsx';
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <BrowserRouter>
       <Routes>
-        <Route element={<Layout/>}>
-          <Route path="/register" element={<UserRegister/>}/>
-          <Route path="/login" element={<UserLogin/>}/>
+        <Route element={<Layout />}>
+          <Route path="/register" element={<UserRegister />} />
+          <Route path="/login" element={<UserLogin />} />
         </Route>
-        <Route path="/dashboard" element={<DashboardLayout/>}>
-          <Route path="contacts" element={<div>Contacts</div>}/>
-          <Route path="users/profile" element={<UserProfile/>}/>
-          <Route path="users/logout" element={<UserLogout/>}/>
+        <Route path="/dashboard" element={<DashboardLayout />}>
+
+          <Route path="users">
+            <Route path="profile" element={<UserProfile />} />
+            <Route path="logout" element={<UserLogout />} />
+          </Route>
+
+          <Route path="contacts">
+            <Route index element={<div>Contacts</div>} />
+            <Route path="create" element={<ContactCreate />} />
+          </Route>
         </Route>
       </Routes>
     </BrowserRouter>
