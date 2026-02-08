839a9520a80c157561549ee399d4989adf532119
sinjitonayo <tanakashinji23@gmail.com>
2026-02-08 21:29:26 +0800

    feat(contact): add contact list component to list existing contact

--- src/lib/api/ContactApi.js
+++ src/lib/api/ContactApi.js
@@ -1,16 +1,36 @@
-export const contactCreate = async (token, {first_name, last_name, email, phone}) => {
+export const contactCreate = async (
+  token,
+  { first_name, last_name, email, phone },
+) => {
   return await fetch(`${import.meta.env.VITE_API_PATH}/contacts`, {
-    method: 'POST',
+    method: "POST",
     headers: {
-      'Content-Type': 'application/json',
-      'Accept': 'application/json',
-      'Authorization': token
+      "Content-Type": "application/json",
+      Accept: "application/json",
+      Authorization: token,
     },
     body: JSON.stringify({
       first_name,
       last_name,
       email,
-      phone
-    })
-  })
-}
+      phone,
+    }),
+  });
+};
+
+export const contactList = async (token, { name, phone, email, page }) => {
+  const url = new URL(`${import.meta.env.VITE_API_PATH}/contacts`);
+
+  if (name) url.searchParams.append("name", name);
+  if (phone) url.searchParams.append("phone", phone);
+  if (email) url.searchParams.append("email", email);
+  if (page) url.searchParams.append("page", page);
+
+  return await fetch(url, {
+    method: "GET",
+    headers: {
+      Accept: "application/json",
+      Authorization: token,
+    },
+  });
+};
--- src/main.jsx
+++ src/main.jsx
@@ -25,7 +25,7 @@ createRoot(document.getElementById('root')).render(
           </Route>
 
           <Route path="contacts">
-            <Route index element={<div>Contacts</div>} />
+            <Route index element={<ContactList/>}/>
             <Route path="create" element={<ContactCreate />} />
           </Route>
         </Route>
