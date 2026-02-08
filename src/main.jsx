5a0e97bf2c7ccbf930b2c8d5ae93038f3c572036
sinjitonayo <tanakashinji23@gmail.com>
2026-02-08 21:36:35 +0800

    feat(contact): add contact detail page and update imports file in main

--- src/main.jsx
+++ src/main.jsx
@@ -8,6 +8,9 @@ import DashboardLayout from "./components/DashboardLayout.jsx";
 import UserProfile from './components/User/UserProfile.jsx';
 import UserLogout from './components/User/UserLogout.jsx';
 import ContactCreate from './components/Contact/ContactCreate.jsx';
+import ContactList from './components/Contact/ContactList.jsx';
+import ContactEdit from './components/Contact/ContactEdit.jsx';
+import ContactDetail from './components/Contact/ContactDetail.jsx';
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
@@ -25,9 +28,10 @@ createRoot(document.getElementById('root')).render(
           </Route>
 
           <Route path="contacts">
-            <Route index element={<ContactList/>}/>
+            <Route index element={<ContactList />} />
             <Route path="create" element={<ContactCreate />} />
-            <Route path=":id/edit" element={<ContactEdit/>}/>
+            <Route path=":id" element={<ContactDetail />} />
+            <Route path=":id/edit" element={<ContactEdit />} />
           </Route>
         </Route>
       </Routes>
